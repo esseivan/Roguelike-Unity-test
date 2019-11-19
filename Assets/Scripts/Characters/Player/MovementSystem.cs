d41be76eb0aa2d93a7cc5cc8b796e84b50acf2b3
EsseivaN <nicolas.esseiva@gmail.com>
2019-11-19 10:06:44 +0100

    2019.3 update

---
+++ Assets/Scripts/Characters/Player/MovementSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementSystem : MonoBehaviour
+{
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3();
+
+        if (Keyboard.current.wKey.isPressed) { movement.z += 1; }
+        if (Keyboard.current.sKey.isPressed) { movement.z -= 1; }
+        if (Keyboard.current.aKey.isPressed) { movement.x += 1; }
+        if (Keyboard.current.dKey.isPressed) { movement.x -= 1; }
+
+
+    }
+}
